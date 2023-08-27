7a224cebe4dd0ebfeb099ab459d18450499be5b4
rongloy <1256765829@qq.com>
2023-08-27 21:28:03 +0800

    Adde interview code

--- permute.py
+++ permute.py
@@ -13,4 +13,95 @@ class  Solution:
 				dfs(i + 1, s-{x})
 		dfs(0, set(nums))
 		return ans
-		
+
+'''
+给你一个数字n，帮我输出所有的有效括号组合
+比如：3
+()()()
+((()))
+()(())
+。。。'''
+
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        ans = []
+        # res = ''
+        def dfs(i : int, leftpar: int, res: str) :
+            if i == 2 * n and leftpar == n:
+                ans.append(res)
+                return 
+            elif i == 2 * n: 
+                return
+            if leftpar < n and leftpar == i-leftpar:
+                res+='('
+                dfs(i + 1, leftpar + 1, res)
+                res = res[:-1]
+            elif leftpar < n and leftpar > i - leftpar:
+                res+='('
+                dfs(i + 1, leftpar + 1, res)  
+                res = res[:-1]
+                res+=')'
+                dfs(i + 1, leftpar, res)
+                res = res[:-1]
+            elif leftpar >= n and leftpar > i - leftpar:
+                res+=')'
+                dfs(i + 1, leftpar, res)
+                res = res[:-1]
+        dfs(0, 0, '')
+        return ans
+
+  // 给你一个循环有序的数组，和一个target，帮我找到这个target在数组的位置。没有就返回-1
+  // 没有重复元素，严格递增
+
+ class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+         
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = (right + left) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] > nums[-1] and nums[mid] > target and target > nums[-1]:
+                right = mid - 1
+            elif nums[mid] > nums[-1]:
+                left = mid + 1
+            elif nums[mid] <= nums[-1] and nums[mid] < target and target <= nums[-1]:
+                left = mid + 1
+            elif nums[mid] <= nums[-1]:
+                right = mid - 1
+        return -1
+// 假设有重复元素，要你找第一个出现的位置。你怎么优化？
+    
+
+    SELECT album_id, SUM(play_secs) AS total_play_secs
+FROM userlog_play_secs
+GROUP BY album_id
+HAVING total_play_secs < (SELECT MAX(total_play_secs) FROM (SELECT album_id, SUM(play_secs) AS total_play_secs FROM userlog_play_secs GROUP BY album_id ORDER BY total_play_secs DESC LIMIT 1) AS top_album)
+ORDER BY total_play_secs DESC
+LIMIT 1;
+
+
+# 2835
+class Solution:
+    def minOperations(self, nums: List[int], target: int) -> int:
+        if target > sum(nums):
+            return -1
+        ct = Counter(nums)
+        ans = 0
+        s = 0
+        i = 0
+        while 1 << i <= target:
+            s += ct[1 << i] << i
+            if s >= (target & ((1 << (i+1)) -1)):
+                i+=1
+                continue
+            i += 1
+            ans += 1
+            while ct[1 << i] == 0 :
+                i += 1
+                ans += 1
+
+        return ans
+                 
