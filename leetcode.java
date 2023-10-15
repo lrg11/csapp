3934d016fcfdc6de2359282f804fb33a65480e75
rongloy <1256765829@qq.com>
2023-10-15 16:09:19 +0800

    Update

--- leetcode.java
+++ leetcode.java
@@ -113,3 +113,36 @@ class Solution {
         return ans;
     }
 }
+
+# validate bst
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root, long left, long right) {
+        if(root == null) {
+            return true;
+        }
+        if (!(left < root.val &&  root.val < right)) return false;
+        return isValidBST(root.left, left, root.val) && isValidBST(root.right, root.val, right);
+    }
+    public boolean isValidBST(TreeNode root) {
+        if(root == null) {
+            return true;
+        }
+    
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+}
