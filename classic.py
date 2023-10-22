1fced960d5c5e9c6ea5d0ef843923bd637e2c242
rongloy <1256765829@qq.com>
2023-10-22 16:02:46 +0800

    Update today code

--- classic.py
+++ classic.py
@@ -780,3 +780,17 @@ class Solution:
         elif Solution.isIPv6(self, IP):
             return "IPv6"
         return "Neither"
+
+
+#
+# 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+#
+# 计算两个数之和
+# @param s string字符串 表示第一个整数
+# @param t string字符串 表示第二个整数
+# @return string字符串
+#
+class Solution:
+    def solve(self , s: str, t: str) -> str:
+        # write code here
+        return str(int(s) + int(t))
